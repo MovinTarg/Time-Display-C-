d4969dbb1f09fa6ad95c04ce835400bf7a9a7570
Pete <pbianchi192@gmail.com>
2018-04-21 10:39:33 -0400

    First commit

---
+++ Controllers/TimeController.cs
@@ -0,0 +1,18 @@
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+ 
+namespace time_display
+{
+    public class TimeController : Controller
+    {
+        [HttpGet]
+        [Route("")]
+        public IActionResult Index()
+        {
+            return View();
+            //OR
+            // return View("Index");
+            //Both of these returns will render the same view (You only need one!)
+        }
+    }
+}
